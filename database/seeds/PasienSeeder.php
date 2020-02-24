61e7efcbf190eddbca493b7d3071b672c8a8338d
budiastra <wayan.budiastra26@gmail.com>
2020-02-24 20:12:43 +0800

    Update Master Pasien

--- database/seeds/PasienSeeder.php
+++ database/seeds/PasienSeeder.php
@@ -17,16 +17,20 @@ class PasienSeeder extends Seeder
         //
         $faker = Faker::create('id_ID');
         
-        for($x=10001; $x <= 20000; $x++){
+        for($x=1; $x <= 2000; $x++){
 
             DB::table('pasien')->insert([
                 'nocm'=> sprintf("%06s", $x),
                 'nama'=> $faker->name,
                 'alamat'=> $faker->address,
                 'tempat_lahir'=> $faker->city,
-                'tgl_lahir'=> '2000-01-01',
+                'tgl_lahir'=> '1990-01-01',
                 'pekerjaan' => 'Investor',
                 'pendidikan' => 'S1',
+                'identitas' => 'KTP',
+                'no_telp' => '12345678',
+                'no_identitas' => '12345678',
+                'aktif'=>'Y'
             ]);
         }
     }
